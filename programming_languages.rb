c8277a2206fcf60beb18de13c99e03a401db45b8
Jamal Shareef <theepie@hotmail.com>
2019-04-08 22:48:34 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,7 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = Hash.new
+  languages.collect do |type, group|
+    group.collect do |name, description|
+      new-hash[name] = 
 end
